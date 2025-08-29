1c8ac057e044a216a6f377b3e0cadf4865133d62
LuisPaulo1 <csluispaulo@gmail.com>
2025-08-28 22:05:26 -0300

    Lidando com erros em um consumer usando Retry Pattern

--- src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
@@ -4,13 +4,10 @@ import com.algaworks.algasensors.temperature.monitoring.api.model.TemperatureLog
 import com.algaworks.algasensors.temperature.monitoring.domain.service.SensorAlertService;
 import com.algaworks.algasensors.temperature.monitoring.domain.service.TemperatureMonitoringService;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
-
-import java.time.Duration;
 
 import static com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq.RabbitMQConfig.QUEUE_ALERTING;
 import static com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq.RabbitMQConfig.QUEUE_PROCESS_TEMPERATURE;
@@ -24,17 +21,13 @@ public class RabbitMQListener {
     private final SensorAlertService sensorAlertService;
 
     @RabbitListener(queues = QUEUE_PROCESS_TEMPERATURE, concurrency = "2-3")
-    @SneakyThrows
     public void handleProcessingTemperature(@Payload TemperatureLogData temperatureLogData) {
         temperatureMonitoringService.processTemperatureReading(temperatureLogData);
-        Thread.sleep(Duration.ofSeconds(5));
     }
 
     @RabbitListener(queues = QUEUE_ALERTING, concurrency = "2-3")
-    @SneakyThrows
     public void handleAlerting(@Payload TemperatureLogData temperatureLogData) {
         sensorAlertService.handleAlert(temperatureLogData);
-        Thread.sleep(Duration.ofSeconds(5));
     }
 
 }
